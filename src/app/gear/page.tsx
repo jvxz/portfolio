2d78842bcfc3526592b12c50cc3a949a46fa9261
jamie <tinycat2004@proton.me>
2024-12-15 20:12:18 -0500

    refactor: :camera_flash: refactored motion & tailwind motion animations

--- src/app/gear/page.tsx
+++ src/app/gear/page.tsx
@@ -2,7 +2,7 @@ import Breadcrumbs from "../components/breadcrumbs";
 
 export default function Page() {
   return (
-    <main className="flex flex-col gap-6">
+    <main className="motion-preset-slide-up-md flex flex-col gap-6">
       <Breadcrumbs />
       <article className="prose prose-neutral dark:prose-invert">
         <h2>hardware</h2>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,49 +2,20 @@ import IndexProfile from "@/features/index/profile";
 import IndexProjects from "@/features/index/projects";
 import MotionParent from "./components/motion/motion-parent";
 import IndexProficiencies from "@/features/index/proficiencies";
-
-const container = {
-  hidden: { opacity: 0 },
-  show: {
-    opacity: 1,
-    transition: {
-      staggerChildren: 0.1,
-    },
-  },
-  exit: {
-    opacity: 0,
-
-    transition: {
-      staggerChildren: 0.1,
-    },
-  },
-};
-
-const item = {
-  hidden: {
-    opacity: 0,
-    y: 27.5,
-    transition: { duration: 0.45, type: "spring" },
-  },
-  show: {
-    opacity: 1,
-    y: 0,
-    transition: { duration: 0.45, type: "spring" },
-  },
-};
+import { fadeStaggerChild, fadeStaggerParent } from "@/lib/def";
 
 export default function Page() {
   return (
     <MotionParent
       className="flex flex-col gap-12"
-      variants={container}
+      variants={fadeStaggerParent}
       initial="hidden"
       animate="show"
       exit="exit"
     >
-      <IndexProfile variants={item} />
-      <IndexProficiencies variants={item} />
-      <IndexProjects variants={item} />
+      <IndexProfile variants={fadeStaggerChild} />
+      <IndexProficiencies variants={fadeStaggerChild} />
+      <IndexProjects variants={fadeStaggerChild} />
     </MotionParent>
   );
 }
