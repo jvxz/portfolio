339a9f7916d4e38a57cf130d6352321e3602c5b5
jamie 🌨️ <jamie@wisp.bio>
2024-12-29 14:59:50 -0500

    feat: :sparkles: add gloss project, update theme, edit project cards

--- src/features/index/projects.tsx
+++ src/features/index/projects.tsx
@@ -27,6 +27,8 @@ import {
   TAGIFY_STACK,
 } from "@/lib/constants";
 import MotionParent from "@/app/components/motion/motion-parent";
+import { Button } from "@/components/ui/button";
+import GithubIcon from "@/app/components/logos/github";
 
 export default function IndexProjects() {
   return (
@@ -47,6 +49,7 @@ export default function IndexProjects() {
             description="gloss is an accessible theme generator for the shadcn/ui library. pick your hue and choose from a variety of styles to generate a theme for your project!"
             href="https://gloss.wisp.bio"
             icon={<Paintbrush className="size-5" />}
+            sourceCode="https://github.com/jvxz/gloss"
           />
         </MotionItem>
         <MotionItem variants={fadeStaggerChild}>
@@ -57,6 +60,7 @@ export default function IndexProjects() {
             description="tagify is a web app that makes it easy for you to edit the id3 metadata tags in your audio files! supports most mainstream audio formats"
             href="https://tagify.wisp.bio"
             icon={<FileAudioIcon className="size-5" />}
+            sourceCode="https://github.com/jvxz/tagify"
           />
         </MotionItem>
         <MotionItem variants={fadeStaggerChild}>
@@ -67,6 +71,7 @@ export default function IndexProjects() {
             description="pinch is a simple web app that allows you to crop your mobile wallpapers to your desired size! supports most mainstream devices, including custom settings"
             href="https://pinch.wisp.bio"
             icon={<TabletSmartphoneIcon className="size-5" />}
+            sourceCode="https://github.com/jvxz/pinch"
           />
         </MotionItem>
       </MotionParent>
@@ -82,6 +87,7 @@ function ProjectCard({
   description,
   href,
   icon,
+  sourceCode,
 }: {
   stack: IconType[];
   title: string;
@@ -89,10 +95,11 @@ function ProjectCard({
   description: string;
   href: string;
   icon: React.ReactNode;
+  sourceCode: string;
 }) {
   return (
     <Card className="transition-colors">
-      <CardHeader>
+      <CardHeader className="flex justify-between">
         <Link
           className="w-fit no-underline underline-offset-4 hover:underline"
           target="_blank"
@@ -110,24 +117,32 @@ function ProjectCard({
         <div className="flex-1">
           <p className="m-0">{description}</p>
         </div>
-        <div className="flex flex-col gap-1">
-          <h3 className="m-0">stack</h3>
-          <div className="flex flex-wrap items-center gap-2 *:m-0 *:h-fit">
-            {stack.map((e) => {
-              return (
-                <Tooltip key={e} delayDuration={100}>
-                  <TooltipTrigger>
-                    <Link href={e} target="_blank">
-                      <Icon icon={e} size={28} />
-                    </Link>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>{e}</p>
-                  </TooltipContent>
-                </Tooltip>
-              );
-            })}
+        <div className="flex items-end justify-between">
+          <div className="flex flex-col gap-1">
+            <h3 className="m-0">stack</h3>
+            <div className="flex flex-wrap items-center gap-2 *:m-0 *:h-fit">
+              {stack.map((e) => {
+                return (
+                  <Tooltip key={e} delayDuration={100}>
+                    <TooltipTrigger>
+                      <Link href={e} target="_blank">
+                        <Icon icon={e} size={28} />
+                      </Link>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>{e}</p>
+                    </TooltipContent>
+                  </Tooltip>
+                );
+              })}
+            </div>
           </div>
+          <Button asChild className="mr-8" variant="link" size="icon">
+            <Link href={sourceCode} target="_blank">
+              <GithubIcon className="size-5" />
+              source code
+            </Link>
+          </Button>
         </div>
       </CardContent>
     </Card>
