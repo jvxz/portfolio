8cf86ae18843b8bcb6648bced4e53100d985973f
jamie 🌨️ <jamie@wisp.bio>
2025-01-25 18:03:45 -0500

    chore: clean slate

--- src/db/actions.ts
+++
@@ -1,9 +0,0 @@
-'use server'
-import { type Message } from "./schema";
-import { db } from "./db";
-import { messagesTable } from "./schema";
-
-export async function createMessage(message: Message) {
-    console.log("attempting to create message: ", message);
-    return db.insert(messagesTable).values(message);
-}
